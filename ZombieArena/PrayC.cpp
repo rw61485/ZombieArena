184e9374484e4c8fe87edb45bd05b1dae1e75857
rw61485 <47870181+rw61485@users.noreply.github.com>
2019-12-16 23:18:43 -0500

    Add files via upload

---
+++ ZombieArena/PrayC.cpp
@@ -0,0 +1,137 @@
+#include "stdafx.h"
+#include "PrayC.h"
+#include "TextureHolder.h"
+
+PrayC::PrayC(int type)
+{
+	// Store the type of this pickup
+	m_Type = type;
+
+	// Associate the texture with the sprite
+	if (m_Type == 1)
+	{
+		m_Sprite = Sprite(TextureHolder::GetTexture(
+			"graphics/green.png"));
+
+		// How much is pickup worth
+		m_Value = HEALTH_START_VALUE;
+
+	}
+	else if (m_Type == 2)
+	{
+		m_Sprite = Sprite(TextureHolder::GetTexture(
+			"graphics/red.png"));
+
+		// How much is pickup worth
+		m_Value = -99;
+	}
+	
+	else if (m_Type == 3)
+	{
+		m_Sprite = Sprite(TextureHolder::GetTexture(
+			"graphics/blue.png"));
+
+		// How much is pickup worth
+		m_Value = 1;
+	}
+
+	m_Sprite.setOrigin(37, 37);
+
+	m_SecondsToLive = START_SECONDS_TO_LIVE;
+	m_SecondsToWait = START_WAIT_TIME;
+}
+
+void PrayC::setArena(IntRect arena)
+{
+
+	// Copy the details of the arena to the pickup's m_Arena
+	m_Arena.left = arena.left + 50;
+	m_Arena.width = arena.width - 50;
+	m_Arena.top = arena.top + 50;
+	m_Arena.height = arena.height - 50;
+
+	spawn();
+}
+
+void PrayC::spawn()
+{
+	// Spawn at a random location
+	srand((int)time(0) / m_Type);
+	int x = (rand() % m_Arena.width);
+	srand((int)time(0) * m_Type);
+	int y = (rand() % m_Arena.height);
+
+	// Not currently spawned
+	//m_Spawned = false;
+	m_SecondsSinceSpawn = 0;
+	m_Spawned = true;
+
+	m_Sprite.setPosition(x, y);
+}
+
+FloatRect PrayC::getPosition()
+{
+	return m_Sprite.getGlobalBounds();
+}
+
+Sprite PrayC::getSprite()
+{
+	return m_Sprite;
+}
+
+bool PrayC::isSpawned()
+{
+	return m_Spawned;
+}
+
+int PrayC::gotIt()
+{
+	m_Spawned = false;
+	m_SecondsSinceDeSpawn = 0;
+	return m_Value;
+}
+
+void PrayC::update(float elapsedTime)
+{
+	if (m_Spawned)
+	{
+		m_SecondsSinceSpawn += elapsedTime;
+	}
+	else
+	{
+		m_SecondsSinceDeSpawn += elapsedTime;
+	}
+
+
+	// Do we need to hide a pickup?
+	if (m_SecondsSinceSpawn > m_SecondsToLive && m_Spawned)
+	{
+		// Revove the pickup and put it somewhere else
+		m_Spawned = false;
+		m_SecondsSinceDeSpawn = 0;
+	}
+
+	// Do we need to spawn a pickup
+	if (m_SecondsSinceDeSpawn > m_SecondsToWait && !m_Spawned)
+	{
+		// spawn the pickup and reset the timer
+		spawn();
+	}
+
+}
+
+void PrayC::upgrade()
+{
+	if (m_Type == 1)
+	{
+		m_Value += (HEALTH_START_VALUE * .5);
+	}
+	else if (m_Type == 2)
+	{
+		m_Value += (HEALTH_START_VALUE * .5);
+	}
+
+	// Make them more frequent and last longer
+	m_SecondsToLive += (START_SECONDS_TO_LIVE / 10);
+	m_SecondsToWait -= (START_WAIT_TIME / 10);
+}
